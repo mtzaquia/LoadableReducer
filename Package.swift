558a61a1139a515ddbb1d621ed0d706c83047253
Mauricio Zaquia <mauriciotzaquia@gmail.com>
2023-09-19 14:05:23 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/pointfreeco/swift-composable-architecture",
-            .exact("1.0.0")
+            .upToNextMajor(from: "1.0.0")
         ),
     ],
     targets: [
